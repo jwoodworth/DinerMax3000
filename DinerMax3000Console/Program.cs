c0ff38018803fcc1748a4142b452e3a157e38737
Jim Woodworth <jgwoodworth@gmail.com>
2018-07-11 01:49:49 -0500

    Add project files.

---
+++ DinerMax3000Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DinerMax3000.Business;
+
+namespace DinerMax3000Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Menu> menusFromDatabase = Menu.GetAllMenus();
+
+            menusFromDatabase = Menu.GetAllMenus();
+
+            Order hungryGuestOrder = new Order();
+
+            foreach (Menu currentMenu in menusFromDatabase)
+            {
+                foreach (MenuItem currentItem in currentMenu.items)
+                {
+                    hungryGuestOrder.items.Add(currentItem);
+                }
+            }
+
+            Console.WriteLine("The total is:" + hungryGuestOrder.Total);
+
+           
+
+
+            Console.ReadKey();
+            
+
+        }
+    }
+}
